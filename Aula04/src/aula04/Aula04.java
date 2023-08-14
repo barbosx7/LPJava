c56bec3f10e63052a0904c1423e610dcb1977271
Ana Barbosa <141193365+barbosx7@users.noreply.github.com>
2023-08-14 20:27:54 -0300

    Add files via upload

---
+++ Aula04/src/aula04/Aula04.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula04;
+
+/**
+ *
+ * @author 15375166631
+ */
+public class Aula04 {
+
+    /**
+ * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        String b = "10";
+        int c = 2;
+        
+        int resultadoA = a + c;
+        int resultadoB = a - c;
+        int resultadoC = a * c;
+        int resultadoD = a / c;
+        int resultadoE = a % c;
+        int resultadoF = (int)a / c;
+        
+        System.out.println (resultadoA);
+        System.out.println(resultadoB);
+        System.out.println(resultadoC);
+        System.out.println(resultadoD);
+        System.out.println(resultadoE);
+        System.out.println(resultadoF);
+     
+    }
+    
+}
